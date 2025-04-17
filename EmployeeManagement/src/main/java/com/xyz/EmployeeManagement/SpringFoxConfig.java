c003ae916edc8f75011128c5d50771a931488f82
agaur.lab <agaur@opentext.com>
2025-04-17 21:47:22 +0530

    Changes for implementing the swagger

---
+++ EmployeeManagement/src/main/java/com/xyz/EmployeeManagement/SpringFoxConfig.java
@@ -0,0 +1,27 @@
+package com.xyz.EmployeeManagement;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API")
+                        .version("1.0")
+                        .description("This is the API documentation for my Spring Boot application"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-apis")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
